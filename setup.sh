289662f630800916aec9259d54a0c4e07443e819
James Young <17939249+jamesy0ung@users.noreply.github.com>
2024-06-29 20:09:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,11 +16,35 @@ sudo sed -i 's/^#\(dtparam=spi=on\)/\1/' /boot/config.txt
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get install --no-install-recommends -y vim cmake libusb-1.0-0-dev rtl-sdr python3-pip python3-venv i2c-tools \
-                        libgpiod-dev python3-libgpiod python3-smbus i2c-tools git build-essential \
-                        aircrack-ng firmware-linux-nonfree firmware-ralink firmware-realtek \
-                        raspberrypi-kernel-headers git libgmp3-dev gawk bison flex make \
-                        autoconf libtool texinfo python3-dev
+sudo apt-get install --no-install-recommends -y \
+    vim \
+    cmake \
+    libusb-1.0-0-dev \
+    rtl-sdr \
+    python3-pip \
+    python3-venv \
+    i2c-tools \
+    libgpiod-dev \
+    python3-libgpiod \
+    python3-smbus \
+    git \
+    build-essential \
+    aircrack-ng \
+    firmware-linux-nonfree \
+    firmware-ralink \
+    firmware-realtek \
+    raspberrypi-kernel-headers \
+    libgmp3-dev \
+    gawk \
+    bison \
+    flex \
+    make \
+    autoconf \
+    libtool \
+    texinfo \
+    python3-dev \
+    python3 \
+    python3-setuptools
 sudo apt purge modemmanager -y
 sudo apt apt autoremove --purge
 sudo apt clean
@@ -31,6 +55,4 @@ git clone https://github.com/F5OEO/rpitx
 python3 -m venv env --system-site-packages
 source env/bin/activate
 echo "source ~/env/bin/activate" >> ~/.bashrc
-pip3 install --upgrade adafruit-python-shell
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-sudo -E env PATH=$PATH python3 raspi-blinka.py
+pip3 install --upgrade adafruit-python-shell RPi.GPIO adafruit-blinka
